20d38d5c302a3a011edb604a8ef9d7b6dbc8f7c7
ttson1001 <ttson1001@gmail.com>
2021-05-12 09:40:15 +0700

    Add project files.

---
+++ ShoppingCart/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Entities
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+    }
+}
